89c4c3e921315b191ef0013d0fa4c92e0583fbaa
r1moo <rlsdeldoc@gmail.com>
2026-04-16 18:11:01 +0800

    Added Level Selector Screen

---
+++ scenes/LevelSelect/level_selector.gd
@@ -0,0 +1,70 @@
+extends Control
+
+# NOTE: Drag and drop your nodes here like we discussed if the paths are different!
+@onready var grid_container = $Overlay/GridContainer
+@onready var play_button = $Overlay/PlayButton 
+
+const LEVEL_TILE_SCENE = preload("res://scenes/LevelSelect/level_tile.tscn")
+const LOCK_TEXTURE = preload("res://assets/boss_splash/Locked_Level.png") # Your lock image
+
+# Load your cool new bubble boss splash art here!
+const BOSS_1_IMAGE = preload("res://assets/boss_splash/BubbleSort_Splash.png")
+
+var level_data = [
+	{"id": 1, "name": "Bubble Sort", "image": BOSS_1_IMAGE},
+	{"id": 2, "name": "???", "image": null},
+	{"id": 3, "name": "???", "image": null},
+	{"id": 4, "name": "???", "image": null},
+	{"id": 5, "name": "???", "image": null},
+	{"id": 6, "name": "???", "image": null}
+]
+# Set to 1 so ONLY the first boss is unlocked
+var highest_unlocked_level = 1 
+var currently_selected_level_id = -1
+var currently_selected_tile = null
+
+func _ready():
+	play_button.disabled = true
+	play_button.pressed.connect(_on_play_button_pressed)
+	generate_level_grid()
+
+func generate_level_grid():
+	# Clear out any placeholder data in the editor
+	for child in grid_container.get_children():
+		child.queue_free()
+		
+	# Spawn exactly 6 tiles
+	for i in range(level_data.size()):
+		var data = level_data[i]
+		var tile = LEVEL_TILE_SCENE.instantiate()
+		grid_container.add_child(tile)
+		
+		# If the ID is greater than 1, it gets locked
+		var is_locked = data.id > highest_unlocked_level
+		
+		tile.setup(data.id, data.name, data.image, is_locked, LOCK_TEXTURE)
+		tile.tile_selected.connect(_on_level_tile_selected)
+		
+		
+func _on_level_tile_selected(level_id, tile_node):
+	if currently_selected_tile == tile_node:
+		currently_selected_tile.set_active_selection(false) # Turn off visuals
+		currently_selected_tile = null                      # Clear the selection
+		currently_selected_level_id = -1                    # Reset the ID
+		play_button.disabled = true                         # Turn off the play button
+		return 
+		
+	if currently_selected_tile != null:
+		currently_selected_tile.set_active_selection(false)
+		
+	currently_selected_tile = tile_node
+	currently_selected_tile.set_active_selection(true)
+	
+	currently_selected_level_id = level_id
+	play_button.disabled = false
+
+func _on_play_button_pressed():
+	if currently_selected_level_id != -1:
+		print("Starting level: ", currently_selected_level_id)
+		# Add your scene transition logic here
+		# e.g., get_tree().change_scene_to_file("res://scenes/levels/level_" + str(currently_selected_level_id) + ".tscn")
