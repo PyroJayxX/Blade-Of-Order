ec2fee59a2b44b9f92c4f59ccf7436c8b358b4be
r1moo <rlsdeldoc@gmail.com>
2026-05-15 06:16:31 +0800

    Added Splash Art for Selection & Shell Sort

--- scripts/GameScripts/level_selector.gd
+++ scripts/GameScripts/level_selector.gd
@@ -8,6 +8,9 @@ extends Control
 const LEVEL_TILE_SCENE = preload("res://scenes/LevelSelect/level_tile.tscn")
 const SPLASH_TEXTURE_BUBBLE = preload("res://assets/boss_splash/BubbleSort_Splash.png")
 const SPLASH_TEXTURE_HEAP = preload("res://assets/boss_splash/HeapSort_Splash.png")
+const SPLASH_TEXTURE_SHELL = preload("res://assets/boss_splash/ShellSort_Splash.png")
+const SPLASH_TEXTURE_SELECTION = preload("res://assets/boss_splash/SelectionSort_Splash.png")
+const SPLASH_TEXTURE_BUCKET = preload("res://assets/boss_splash/BucketSort_Splash.png")
 
 var level_data: Array[Resource] = []
 var currently_selected_level_id = -1
